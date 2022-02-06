84d4cf9c7d7465f0fff3aa4c62c69506740ced36
vbourassa <bourassa.v@gmail.com>
2022-02-06 21:06:49 +0100

    Going through chapter 4, maybe I should redo chapter 2. Can't remember how I calculated the outliers

--- src/chap4_field.py
+++ src/chap4_field.py
@@ -8,7 +8,8 @@ import plotly.graph_objects as go
 import seaborn as sns
 from scipy import stats
 import numpy as np
-from plotnine import ggplot, aes, geom_line, geom_point
+from plotnine import ggplot, aes, geom_line, geom_point, labs, geom_smooth
+from plotnine import geom_histogram, geom_boxplot
 
 # %% ANCHOR Sources files
 
@@ -39,6 +40,7 @@ fig.show()
 # And... It works great!
 gg_facebook = ggplot(facebook_data, aes(x="NPQC_R_Total", y="Rating"))
 gg_facebook + geom_point(aes(color="Rating_Type"), size=1, position="jitter")
+gg_facebook + geom_point(aes(shape="Rating_Type"), size=1, position="jitter")
 
 
 # %% ANCHOR 4.5.1 Graphing relationships: the scatterplot
@@ -60,15 +62,75 @@ fig = px.scatter(
 
 fig.show()
 
-# %% ANCHOR 4.5.2 Adding a funky line
+# This is the ggplot version, love plotnine
+scat_anxiety = ggplot(df_anxiety, aes("Anxiety", "Exam"))
+scat_anxiety + geom_point() + labs(x="Exam Anxiety", y="Exam Performance %")
 
-(
-    ggplot(
-        df_anxiety,
-        aes(
-            "Anxiety",
-            "Exam"
-        )
-    )
-    + geom_point()
-)
+# %% ANCHOR 4.5.2 Adding a funky line
+# From now on, I'm using plotnine
+
+scat_anxiety + geom_point() + geom_smooth(
+    method="lm",
+    alpha=0.1,
+    fill="#448aff",
+    color="#448aff") + labs(
+        x="Exam Anxiety",
+        y="Exam Performance %")
+
+# %% ANCHOR 4.5.3 Grouped scatterplot
+
+scatter = ggplot(df_anxiety, aes(
+    "Anxiety",
+    "Exam",
+    color="Gender"
+))
+
+# The order is important fort aesthetics in plotnine
+scatter + geom_point() + geom_smooth(aes(fill="Gender"), method="lm")
+
+# %% ANCHOR 4.5.3 Self-test
+# Go back to the Facebook narcissism data from the earlier tutorial. Plot a
+# graph that shows the pattern in the data using only a line. 
+
+df_facebook = facebook_data
+
+
+line = ggplot(df_facebook, aes(x="NPQC_R_Total", y="Rating"))
+line + geom_smooth(method="lm")
+
+
+# Plot different coloured lines for the different types of rating (cool,
+# fashionable, attractive, glamorous).
+
+# I could've had labs, but which one?
+line + geom_smooth(aes(color="Rating_Type"), method="lm", se=False)
+line + geom_smooth(
+    aes(color="Rating_Type"),
+    method="lm",
+    se=False) + geom_point(
+        aes(color="Rating_Type"),
+        position="jitter")
+
+# %% 4.6 Histograms : a good way to spot obvious problems
+
+df_festival = pd.read_csv(src_festival, sep="\t")
+
+# NOTE I haven't find a way to use ggplot opts
+
+histogram = ggplot(df_festival, aes(x="day1"))
+histogram + geom_histogram()
+
+(histogram
+ + geom_histogram(binwidth=0.4)
+ + labs(
+     x="Hygiene (Day 1 of Festival)",
+     y="Frequency"))
+
+# %% ANCHOR 4.1 Jane superbrain
+
+
+
+# %% ANCHOR 4.7 Boxplots (box-whisker diagrams)
+
+boxplot = ggplot(df_festival, aes("gender", "day1"))
+boxplot + geom_boxplot()
